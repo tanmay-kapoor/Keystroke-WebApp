458288ef1cc79fd044914f1fbea5abc6dd09a1ed
Tanmay Kapoor <tanmay.skater@gmail.com>
2022-03-13 12:32:57 +0530

    minor change for aws

--- server.js
+++ server.js
@@ -23,4 +23,6 @@ const routes = require("./routes/routes.js");
 
 app.use("/", routes);
 
-app.listen("3000", () => console.log("Server started on port 3000"));
+const port =
+    process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
