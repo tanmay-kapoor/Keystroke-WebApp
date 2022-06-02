e6b224d8b467b48d186416c685fb8aed0f629339
Tanmay Kapoor <tanmay.skater@gmail.com>
2022-06-02 09:15:40 +0530

    only show stress level as 0/1

--- helpers/predict.py
+++ helpers/predict.py
@@ -6,6 +6,7 @@ import pickle
 
 model = pickle.load(open("model.pkl", "rb"))
 
+og = pd.read_csv("entries.csv")
 keystroke_data = pd.read_csv("entries.csv")
 keystroke_data = keystroke_data.drop(['username', 'stressLevel'], 1)
 
@@ -19,6 +20,12 @@ features_pca = pca.transform(scaled_features)
 preds = []
 for i in range(len(features_pca)):
     stressLevel = model.predict([features_pca[i]])
-    preds.append(stressLevel[0])
+    actual = 1
+    if(og.iloc[i]["stressLevel"] == 0):
+        actual = 0
 
-print(preds)
+    preds.append(
+        {"actual": actual, "predicted": stressLevel[0]})
+
+for pair in preds:
+    print(pair)
